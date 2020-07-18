3c003c97361114436364d376bfe521b871451a60
ozorob2 <omar.zorob@yahoo.com>
2020-07-18 01:49:01 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -8,9 +8,8 @@ sed -i '/home/d' /etc/fstab
 umount /dev/sda1
 umount /dev/sda3
 
+mkdir ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqfal6XdwNCSAnWsIA73mWBA0mdF26bKdcDB4MpVHEypomd69iEjNO0Vdx+ut+Y+kf3K6aBTd8vg9WoqpMpv4lnwo0iOsH6KMOeFnB9Pj22GFQaFtum7VGRb1RQ9u6gg5hixYe2Yk1ui7LgfZUQqqyh2PkaBeFpXoeYOWYvXsOxMOqtVAF6K0g1dYJEL0VeYDxHQlP8CAuQv1KkUqYDxkJSg9U+iO+Tjvez6aau/Nw6ynXdlyZxUX+om/xspInUJy9sAimvQ3icrC5EMFyKJica0eVB48VfGuKFyRnIbVAUhXBfSJ+n4s9waUdW+RhlNcxh9Qgqs3snbqaC9ipDB3n omarzorob@Omars-MacBook-Pro.local" >> ~/.ssh/authorized_keys
 
 sed -e 's|overlayroot=""|overlayroot="device:dev=/dev/sda3,timeout=180"|' /etc/overlayroot.conf > tmp.txt
 cp tmp.txt /etc/overlayroot.conf
-
-reboot
